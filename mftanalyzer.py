a0d93da4a3914c17be30bdc737783dde912b2a66
cyberyom <132921492+cyberyom@users.noreply.github.com>
2023-12-31 18:25:36 -0500

    Update mftanalyzer.py

--- mftanalyzer.py
+++ mftanalyzer.py
@@ -7,36 +7,25 @@ in the MFT file entry such as x-ways templates will.
 
 Program lives on github.
 """ 
-import sys
-import os
 from prettytable import PrettyTable
 from datetime import datetime, timedelta
-from mftclasses import StandardInfo
-from mftclasses import
-from mftclasses import
-from mftclasses import
-from mftclasses import
-from mftclasses import
-from mftclasses import
-from mftclasses import
-from mftclasses import
-from mftclasses import
-from mftclasses import
+import os
+import sys
+import struct
 
 print("\033[91m" + """
-		M   M  FFFFF  TTTTT  
-		MM MM  F        T    
-		M M M  FFF      T    
-		M   M  F        T    
-		M   M  F        T    
+        M   M  FFFFF  TTTTT  
+        MM MM  F        T    
+        M M M  FFF      T    
+        M   M  F        T    
+        M   M  F        T    
 """ + "\033[92m" + """
 AAAAA  N   N  AAAAA  L     Y   Y  ZZZZZ  EEEEE  RRRRR  
 A   A  NN  N  A   A  L      Y Y      Z    E      R   R 
 AAAAA  N N N  AAAAA  L       Y      Z     EEEE   RRRR  
 A   A  N  NN  A   A  L       Y     Z      E      R R   
 A   A  N   N  A   A  LLLLL   Y     ZZZZZ  EEEEE  R  RR 
-""" + "\033[0m" + "	by CyberYom\n\n")
-
+""" + "\033[0m" + "      by CyberYom\n\n")
 
 def firstrun():
     print('Welcome to MFT Analyzer. This tool is designed to parse and display MFT metadata. \nPassing -h will display a help menu.' + '\n\n')
@@ -62,6 +51,108 @@ def convert_hex_timestamp_to_datetime(hex_timestamp):
     except (ValueError, OverflowError):
         return "Invalid Timestamp"
 
+def hex_to_ascii(hex_bytes):
+    ascii_str = ''
+    for hex_byte in hex_bytes:
+        byte_int = int(hex_byte, 16)
+        if byte_int == 0:
+            ascii_str += ''
+        else:
+            ascii_str += chr(byte_int) if 32 <= byte_int < 127 else '.'
+    return ascii_str
+
+def hex_to_short(hex_str):
+    try:
+        # Reverse the byte order in the hex string
+        reversed_hex_str = ''.join(reversed([hex_str[i:i+2] for i in range(0, len(hex_str), 2)]))
+
+        decimal_value = int(reversed_hex_str, 16)
+        
+        # Check if the decimal value is too large to fit in a short
+        if decimal_value > 32767 or decimal_value < -32768:
+            return "Out of range for short"
+
+        return decimal_value
+    except (ValueError, OverflowError):
+        return "Invalid Hex Value"
+
+def filetime_to_dt(filetime_bytes):
+    filetime_int = struct.unpack('<Q', filetime_bytes)[0]
+    windows_epoch = datetime(1601, 1, 1)
+
+    return windows_epoch + timedelta(microseconds=filetime_int // 10)
+
+def bytes_to_uint32(hex_str):
+    if not isinstance(hex_str, str):
+        raise TypeError("Expected hex_str to be a string")
+
+    byte_data = bytes.fromhex(hex_str)
+    return int.from_bytes(byte_data, byteorder='little')
+
+
+
+def bytes_to_hex(byte_data):
+    # Reverse the byte_data list for little-endian order
+    byte_data_reversed = byte_data[::-1]
+
+    # Convert each byte to a hex string and concatenate
+    hex_string = ''.join(format(int(byte, 16), '02X') for byte in byte_data_reversed if byte != '00')
+    return hex_string
+
+def hex_to_uint(hex_str):
+    try:
+        # Reverse the hexadecimal string in pairs of two characters
+        reversed_hex_str = ''.join(reversed([hex_str[i:i+2] for i in range(0, len(hex_str), 2)]))
+        
+        decimal_value = int(reversed_hex_str, 16)
+        
+        # Ensure the decimal value is non-negative
+        if decimal_value < 0:
+            return "Negative value not allowed for uint"
+
+        return decimal_value
+    except (ValueError, OverflowError):
+        return "Invalid Hex Value"
+
+def bytes_to_uint64(raw_bytes):
+    # Ensure that the input has exactly 8 bytes (64 bits)
+    if len(raw_bytes) != 8:
+        raise ValueError("Input must be exactly 8 bytes long")
+
+    # Use struct.unpack to convert the bytes to a uint64
+    uint64_value = struct.unpack('Q', raw_bytes)[0]
+    return uint64_value
+
+def bytes_to_decimal(hex_string_list):
+    byte_data = bytes.fromhex(''.join(hex_string_list))
+    return int.from_bytes(byte_data, byteorder='little')
+
+def residency(hex_dump):
+    if len(hex_dump) != 1:
+        return "Invalid input length"
+
+    if hex_dump[0] == '00':
+        return "Resident"
+    elif hex_dump[0] == '01':
+        return "Nonresident"
+    else:
+        return "Invalid residency byte"
+
+def dataflag(hex_dump):
+    if len(hex_dump) != 2:
+        return "Invalid input length"
+
+    if hex_dump[1] == '01':
+        return "Is Compressed"
+    elif hex_dump[1] == 'ff':
+        return "-"
+    elif hex_dump[0] == '40':
+        return "Is Encrypted"
+    elif hex_dump[0] == '80':
+        return "Is Sparse"
+    else:
+        return "Unknown Attr. data flag bytes"
+
 
 def timestamp_from_hex_dump(hex_dump):
     hex_timestamp = ''.join(hex_dump)
@@ -73,8 +164,6 @@ def timestamp_from_hex_dump(hex_dump):
     else:
         # Otherwise, format the datetime object
         return datetime_obj.strftime('%Y-%m-%d %H:%M:%S')
-
-
 
 def MFT(argpath, target_bytes, length_bytes):
     outputs = []  # A list to store hex dumps for each occurrence
@@ -107,96 +196,126 @@ def MFT(argpath, target_bytes, length_bytes):
     except IOError:
         return ['Error reading the file.']
 
-
-def parse_attribute(attr_data):
-    attr_type = int.from_bytes(attr_data[0:4], byteorder='little')
-    # Add logic to handle different types of attributes
-    # This is a placeholder implementation and should be expanded based on the MFT structure
-    if attr_type == 0x10:  # Example: $STANDARD_INFORMATION
-        return [("Type", "$STANDARD_INFORMATION"), ("Timestamp", "Example Timestamp")]
-    elif attr_type == 0x20:
-        return [("Type", "$ATTRIBUTE_LIST"), ("x", "x")]
-    elif attr_type == 0x30:  # Example: $FILE_NAME
-        return [("Type", "$FILE_NAME"), ("File Name", "Example Filename")]
-    elif attr_type == 0x40:
-        return [("Type", "$OBJECT_ID"), ("x", "x")]
-    elif attr_type == 0x60:
-        return [("Type", "$VOLUME_NAME"), ("x", "x")]
-    elif attr_type == 0x70:
-        return [("Type", "VOLUME_INFORMATION"), ("x", "x")]
-    elif attr_type == 0x80:
-        return [("Type", "$DATA"), ("x", "x")]
-    elif attr_type == 0x90:
-        return [("Type", "$INDEX_ROOT"), ("x", "x")]
-    elif attr_type == 0xA0:
-        return [("Type", "$INDEX_ALLOCATION"), ("x", "x")]
-    elif attr_type == 0xB0:
-        return [("Type", "$BITMAP"), ("x", "x")]
-    elif attr_type == 0xC0:
-        return [("Type", "$REPARSE_POINT"), ("x", "x")]
-    else:
-        return [("Type", f"Unknown (0x{attr_type:04X})"), ("Data", "Not parsed")]
-
 def handle_path(providedpath):
-    result = ""
     if os.path.exists(providedpath):
-        try:
-            with open(providedpath, 'rb') as file:
-                entry_number = 0
-                while True:
-                    entry = file.read(1024)  # Assuming 1024 bytes per MFT entry
-                    if not entry:
-                        break
+        target_bytes = b'\x46\x49\x4C\x45'
+        all_hex_dumps = MFT(providedpath, target_bytes, 1024)
 
-                    table = PrettyTable()
-                    table.field_names = ["Title", "Data"]
+        if all_hex_dumps and all_hex_dumps != ["Target byte sequence not found."]:
+            all_tables = ""
 
-                    # Parse the entry and add rows to the table
-                    first_attr_offset = int.from_bytes(entry[20:22], byteorder='little')
-                    offset = first_attr_offset
-                    while offset < 1024:
-                        attr_length = int.from_bytes(entry[offset+4:offset+8], byteorder='little')
-                        if attr_length == 0:
-                            break  # End of attributes or invalid length
+            for hex_dump in all_hex_dumps:
+                if len(hex_dump) >= 6:
+                    print("File:")
+                    if b'\x46\x49\x4C\x45' in bytes.fromhex(''.join(hex_dump)):
+                        print(Entry_Header(hex_dump))
+                        standardinfo_offset = hex_to_short(''.join(hex_dump[20:22])) #grabs the value of net attribute offset and binds to new_offset
 
-                        attr_data = entry[offset:offset+attr_length]
-                        parsed_data = parse_attribute(attr_data)
+                    if bytes.fromhex(''.join(hex_dump[standardinfo_offset:standardinfo_offset+4])) == b'\x10\x00\x00\x00':
+                        print('      $Standard Information Attribute')
+                        standard_info(hex_dump[standardinfo_offset:])
+                        next_offset1 = standardinfo_offset + int(bytes_to_hex(hex_dump[standardinfo_offset+4:standardinfo_offset+8]), 16)
 
-                        for title, data in parsed_data:
-                            table.add_row([title, data])
+                    if bytes.fromhex(''.join(hex_dump[next_offset1:next_offset1+4])) == b'\x20\x00\x00\x00':
+                        print('      $Attribute List Attribute')
 
-                        offset += attr_length
+                    if bytes.fromhex(''.join(hex_dump[next_offset1:next_offset1+4])) == b'\x30\x00\x00\x00':
+                        print('      $File Name Attribute')
+                        file_name(hex_dump[])
 
-                    result += f"Entry {entry_number}\n" + table.get_string() + "\n\n"
-                    entry_number += 1
 
-        except FileNotFoundError:
-            return 'File not found.'
-        except IOError as e:
-            return f'Error reading the file: {e}'
-        except Exception as e:
-            return f'An unexpected error occurred: {e}'
+                else:
+                    all_tables += "Hex dump is too short.\n\n"
+
+            return all_tables.rstrip()  # Return all tables as a single string
+        else:
+            return "Target byte sequence not found." if all_hex_dumps == ["Target byte sequence not found."] else "Hex dump is too short."
     else:
-        return 'Provided path does not exist.'
-    return result
+        return 'File not found.'
 
+
+def Entry_Header(hex_dump):
+    print('      Entry Header')
+    table = PrettyTable()
+    table.field_names = ["Title", "Raw Data", "Data"]
+
+    table.add_row(["Signature", ' '.join(hex_dump[:4]),hex_to_ascii(hex_dump[:4])]) # Use the first 4 bytes for Signature
+    table.add_row(["Update Sequence Offset", ' '.join(hex_dump[4:6]), hex_to_short(''.join(hex_dump[4:6]))])
+    table.add_row(["Update Sequence Size", ' '.join(hex_dump[6:8]), hex_to_short(''.join(hex_dump[6:8]))])
+    table.add_row(["Logfile Sequence Number", ' '.join(hex_dump[8:16]), bytes_to_uint64(bytes.fromhex(''.join(hex_dump[8:16])))])
+    table.add_row(["Use/Deletion Count", ' '.join(hex_dump[16:18]), hex_to_short(''.join(hex_dump[16:18]))])
+    table.add_row(["Hard-link Count", ' '.join(hex_dump[18:20]), hex_to_short(''.join(hex_dump[18:20]))])
+    table.add_row(["Offset to First Attribute", ' '.join(hex_dump[20:22]), hex_to_short(''.join(hex_dump[20:22]))])
+    table.add_row(["Flags", ' '.join(hex_dump[22:24]), hex_to_ascii(hex_dump[22:24])])
+    table.add_row(["Logical Size of Record", ' '.join(hex_dump[24:28]), hex_to_uint(''.join(hex_dump[24:28]))])
+    table.add_row(["Physical Size of Record", ' '.join(hex_dump[28:32]), hex_to_uint(''.join(hex_dump[28:32]))])
+    table.add_row(["Base Record", ' '.join(hex_dump[32:40]), bytes_to_uint64(bytes.fromhex(''.join(hex_dump[32:40])))])
+    return table.get_string() + "\n\n"
+
+def standard_info(hex_dump):
+    table = PrettyTable()
+    table.field_names = ["Title", "Raw Data", "Data"] #the tables start with generic attr header
+    table.add_row(["Attribute Type", ' '.join(hex_dump[0:4]), "$Standard Information"])
+    table.add_row(["Attribute Size", ' '.join(hex_dump[4:8]), bytes_to_hex(hex_dump[4:8])])
+    table.add_row(["Attribute Residency", ' '.join(hex_dump[8:9]), residency(hex_dump[8:9])])
+    table.add_row(["Name Size", ' '.join(hex_dump[9:10]), hex_dump[9:10]])
+    table.add_row(["Name Offset", ' '.join(hex_dump[10:12]), hex_to_short(''.join(hex_dump[10:12]))])
+    table.add_row(["Attr. Data Flags", ' '.join(hex_dump[12:14]), dataflag(hex_dump[12:14])])
+    table.add_row(["Attr. ID", ' '.join(hex_dump[14:16]), hex_to_short(''.join(hex_dump[14:16]))])
+    #end of shared attr header
+    table.add_row(["File Creation",' '.join(hex_dump[24:32]), filetime_to_dt(bytes([int(b, 16) for b in hex_dump[24:32]]))])
+    table.add_row(["File Modification",' '.join(hex_dump[32:40]), filetime_to_dt(bytes([int(b, 16) for b in hex_dump[32:40]]))])
+    table.add_row(["MFT Modification",' '.join(hex_dump[40:48]), filetime_to_dt(bytes([int(b, 16) for b in hex_dump[40:48]]))])
+    table.add_row(["File Accessed",' '.join(hex_dump[48:56]), filetime_to_dt(bytes([int(b, 16) for b in hex_dump[48:56]]))])
+    table.add_row(["Attribute Flags", ' '.join(hex_dump[56:60]), bytes_to_hex(hex_dump[56:60])])   #needs a function to parse flags. Check documentation
+    table.add_row(["Max Versions", ' '.join(hex_dump[60:64]), "Unknown"])
+    table.add_row(["Version Number", ' '.join(hex_dump[64:68]), "Unknown"])
+    table.add_row(["Class Identifier", ' '.join(hex_dump[68:72]), "Unknown"])
+    table.add_row(["Owner Identifier", ' '.join(hex_dump[72:76]), "Unknown"])
+    table.add_row(["Security Identifier", ' '.join(hex_dump[76:80]), bytes_to_decimal(hex_dump[76:80])])
+    table.add_row(["Quota Charged", ' '.join(hex_dump[80:88]), "Unknown"])
+    table.add_row(["Update Sequence Number", ' '.join(hex_dump[88:96]), "Unknown"])
+    print(table)
+
+
+def attirbute_list(hex_dump):
+    print('      $Attribute List Attribute')
+    table = PrettyTable()
+    table.field_names = ["Title", "Raw Data", "Data"]
+    table.add_row(["Attribute Type", ' '.join(hex_dump[0:4]), "$Attribute List"])
+    table.add_row(["Attribute Size", ' '.join(hex_dump[4:8]), bytes_to_hex(hex_dump[4:8])])
+    table.add_row(["Attribute Residency", ' '.join(hex_dump[8:9]), residency(hex_dump[8:9])])
+    table.add_row(["Name Size", ' '.join(hex_dump[9:10]), hex_dump[9:10]])
+    table.add_row(["Name Offset", ' '.join(hex_dump[10:12]), hex_to_short(''.join(hex_dump[10:12]))])
+    table.add_row(["Attr. Data Flags", ' '.join(hex_dump[12:14]), dataflag(hex_dump[12:14])])
+    table.add_row(["Attr. ID", ' '.join(hex_dump[14:16]), hex_to_short(''.join(hex_dump[14:16]))])
+
+    print(table)
+
+def file_name(hex_dump):
+    print('      $Attribute List Attribute')
+    table = PrettyTable()
+    table.field_names = ["Title", "Raw Data", "Data"]
+    table.add_row(["Attribute Type", ' '.join(hex_dump[0:4]), "$File Name"])
+    table.add_row(["Attribute Size", ' '.join(hex_dump[4:8]), bytes_to_hex(hex_dump[4:8])])
+    table.add_row(["Attribute Residency", ' '.join(hex_dump[8:9]), residency(hex_dump[8:9])])
+    table.add_row(["Name Size", ' '.join(hex_dump[9:10]), hex_dump[9:10]])
+    table.add_row(["Name Offset", ' '.join(hex_dump[10:12]), hex_to_short(''.join(hex_dump[10:12]))])
+    table.add_row(["Attr. Data Flags", ' '.join(hex_dump[12:14]), dataflag(hex_dump[12:14])])
+    table.add_row(["Attr. ID", ' '.join(hex_dump[14:16]), hex_to_short(''.join(hex_dump[14:16]))])
+
+    print(table)
 
 def output_results(data, outputpath):
     try:
         with open(outputpath, 'w') as file:
-            file.write(str(data))
+            file.write(data)
         print(f'Output written to file {outputpath}')
     except IOError as e:
         print(f"An error occurred while writing to the file: {e}")
 
-def hex_to_ascii(hex_bytes):
-    ascii_str = ''
-    for hex_byte in hex_bytes:
-        byte_int = int(hex_byte, 16)
-        ascii_str += chr(byte_int) if 32 <= byte_int < 127 else '.'
-    return ascii_str
-
 # Script Execution
+
 if len(sys.argv) == 1:
     firstrun()
 elif '-h' in sys.argv:
