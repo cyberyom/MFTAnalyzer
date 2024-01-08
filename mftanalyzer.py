f0896a59cbb233c632ddb5a9037c179e09964aeb
cyberyom <132921492+cyberyom@users.noreply.github.com>
2024-01-07 22:08:39 -0500

    Update mftanalyzer.py
    Added file entry serching anf csv output

--- mftanalyzer.py
+++ mftanalyzer.py
@@ -12,6 +12,7 @@ from mfttemplate import tablecreation
 from mfttemplate import logic
 import os
 import sys
+import csv
 
 def MFT(argpath, target_bytes):
     outputs = [] 
@@ -23,7 +24,7 @@ def MFT(argpath, target_bytes):
             while True:
                 start_offset = file_content.find(target_bytes, offset)
                 if start_offset == -1:
-                    break  # No more occurrences found
+                    break
 
                 logical_size_bytes = file_content[start_offset+24:start_offset+28]
                 logical_size = int.from_bytes(logical_size_bytes, byteorder='little')
@@ -76,21 +77,27 @@ def update_offset(hex_dump, current_offset):
     attr_length_hex = logic_instance.bytes_to_decimal(hex_dump[current_offset+4:current_offset+8])
     return current_offset + attr_length_hex
 
-def handle_path(providedpath):
+def handle_path(providedpath, search_name=None, export_csv=False):
     if os.path.exists(providedpath):
         target_bytes = b'\x46\x49\x4C\x45'  # FILE signature
         all_hex_dumps = MFT(providedpath, target_bytes)
 
         if not all_hex_dumps or all_hex_dumps == ["No MFT entries found."]:
             return "No MFT entries found."
+
         all_tables = ""
+        all_pretty_tables = []
+        tablecreation_instance = tablecreation()
 
         for hex_dump in all_hex_dumps:
+            extracted_name = None
             logic_instance = logic()
-            entry_table = tablecreation.Entry_Header(hex_dump[0:]) 
+            pretty_table = tablecreation_instance.Entry_Header(hex_dump[0:])
+            all_pretty_tables.append(pretty_table)
             logical_size = logic_instance.hex_to_uint(''.join(hex_dump[24:28]))
             current_offset = logic_instance.hex_to_short(''.join(hex_dump[20:22]))
             previous_offset = None
+            entry_table = ""  # Initialize entry_table as an empty string
 
             while current_offset < len(hex_dump) and current_offset < logical_size:
                 if current_offset == previous_offset:
@@ -98,55 +105,144 @@ def handle_path(providedpath):
 
                 attr_type = determine_attribute_type(hex_dump, current_offset)
 
+                sd_table = None
+                si_table = None
+                al_table = None
+                fn_table = None
+                oi_table = None
+                vn_table = None
+                vii_table = None
+                da_table = None
+                ir_table = None
+                ia_table = None
+                bm_table = None
+                sl_table = None
+                rp_table = None
+                eai_table = None
+                ea_table = None
+                ps_table = None
+                lus_table = None
+
                 if attr_type != "Unknown":
                     offset_info = f"    Attribute Type: {attr_type}, Current Offset: {current_offset} \n"
                     entry_table += offset_info
-                    tablecreation_instance = tablecreation()
 
+                    # Ensure each method call returns a PrettyTable, then get its string representation
                     if attr_type == '$STANDARD_INFORMATION':
-                        entry_table += tablecreation_instance.standard_info(hex_dump[current_offset:])
+                        si_table = tablecreation_instance.standard_info(hex_dump[current_offset:])
+                        all_pretty_tables.append(si_table)
+                        entry_table += si_table.get_string() + "\n"
+
+
                     elif attr_type == '$ATTRIBUTE_LIST':
-                        entry_table += tablecreation_instance.attirbute_list(hex_dump[current_offset:])
-                    elif attr_type == '$FILE_NAME':
-                        entry_table += tablecreation_instance.file_name(hex_dump[current_offset:])
-                    elif attr_type == '$VOLUME_VERSION':
-                        entry_table += tablecreation_instance.volume_version(hex_dump[current_offset:])
+                        al_table = tablecreation_instance.attribute_list(hex_dump[current_offset:])
+                        all_pretty_tables.append(al_table)
+                        entry_table += al_table.get_string() + "\n"
+
+
+                    if attr_type == '$FILE_NAME':
+                        fn_table = tablecreation_instance.file_name(hex_dump[current_offset:])
+                        all_pretty_tables.append(fn_table)
+                        entry_table += fn_table.get_string() + "\n"
+                        namesize_hex = logic_instance.bytes_to_hex(hex_dump[current_offset+88:current_offset+89])
+                        namesize = int(namesize_hex, 16) * 2  # Multiply by 2 as each character is represented by 2 bytes
+                        filename_hex_dump = hex_dump[current_offset+90:current_offset+90+namesize]
+                        extracted_name = logic_instance.extract_filename(filename_hex_dump)
+
                     elif attr_type == '$OBJECT_ID':
-                        entry_table += tablecreation_instance.object_id(hex_dump[current_offset:])
+                        oi_table = tablecreation_instance.object_id(hex_dump[current_offset:])
+                        if oi_table is not None:
+                            all_pretty_tables.append(oi_table)
+                            entry_table += oi_table.get_string() + "\n"
+
                     elif attr_type == '$SECURITY_DESCRIPTOR':
-                        entry_table += tablecreation_instance.security_descriptor(hex_dump[current_offset:])
+                        sd_table = tablecreation_instance.security_descriptor(hex_dump[current_offset:])
+                        if sd_table is not None:
+                            all_pretty_tables.append(sd_table)
+                            entry_table += sd_table.get_string() + "\n"
+
                     elif attr_type == "$VOLUME_NAME":
-                        entry_table += tablecreation_instance.volume_name(hex_dump[current_offset:])
+                        vn_table = tablecreation_instance.volume_name(hex_dump[current_offset:])
+                        if vn_table is not None:
+                            all_pretty_tables.append(vn_table)
+                            entry_table += vn_table.get_string() + "\n"
+
                     elif attr_type == '$VOLUME_INFORMATION':
-                        entry_table += tablecreation_instance.volume_information(hex_dump[current_offset:])
+                        vii_table = tablecreation_instance.volume_information(hex_dump[current_offset:])
+                        all_pretty_tables.append(vii_table)
+                        entry_table += vii_table.get_string() + "\n"
+
                     elif attr_type == '$DATA':
-                        entry_table += tablecreation_instance.data(hex_dump[current_offset:])
+                        da_table = tablecreation_instance.data(hex_dump[current_offset:])
+                        all_pretty_tables.append(da_table)
+                        entry_table += da_table.get_string() + "\n"
+
                     elif attr_type == '$INDEX_ROOT':
-                        entry_table += tablecreation_instance.index_root(hex_dump[current_offset:])
+                        ir_table = tablecreation_instance.index_root(hex_dump[current_offset:])
+                        all_pretty_tables.append(ir_table)
+                        entry_table += ir_table.get_string() + "\n"
+
                     elif attr_type == '$INDEX_ALLOCATION':
-                        entry_table += tablecreation_instance.index_allocation(hex_dump[current_offset:])
+                        ia_table = tablecreation_instance.index_allocation(hex_dump[current_offset:])
+                        all_pretty_tables.append(ia_table)
+                        entry_table += ia_table.get_string() + "\n"
+
                     elif attr_type == '$BITMAP':
-                        entry_table += tablecreation_instance.bitmap(hex_dump[current_offset:])
+                        bm_table = tablecreation_instance.bitmap(hex_dump[current_offset:])
+                        all_pretty_tables.append(bm_table)
+                        entry_table += bm_table.get_string() + "\n"
+
                     elif attr_type == '$SYMBOLIC_LINK':
-                        entry_table += tablecreation_instance.symbolic_link(hex_dump[current_offset])
+                        sl_table = tablecreation_instance.index_root(hex_dump[current_offset:])
+                        all_pretty_tables.append(sl_table)
+                        entry_table += sl_table.get_string() + "\n"
+
                     elif attr_type == '$REPARSE_POINT':
-                        entry_table += tablecreation_instance.reparse_point(hex_dump[current_offset:])
+                        rp_table = tablecreation_instance.reparse_point(hex_dump[current_offset:])
+                        all_pretty_tables.append(rp_table)
+                        entry_table += rp_table.get_string() + "\n"
+
                     elif attr_type == '$EA_INFORMATION':
-                        entry_table =+ tablecreation_instance.ea_information(hex_dump[current_offset])
+                        eai_table = tablecreation_instance.ea_information(hex_dump[current_offset:])
+                        all_pretty_tables.append(eai_table)
+                        entry_table += eai_table.get_string() + "\n"
+
                     elif attr_type == '$EA':
-                        entry_table += tablecreation_instance.ea(hex_dump[current_offset:])
+                        ea_table = tablecreation_instance.ea(hex_dump[current_offset:])
+                        all_pretty_tables.append(ea_table)
+                        entry_table += ea_table.get_string() + "\n"
+
                     elif attr_type == 'PROPERTY_SET':
-                        entry_table += tablecreation_instance.property_set(hex_dump[current_offset:])
+                        ps_table = tablecreation_instance.property_set(hex_dump[current_offset:])
+                        all_pretty_tables.append(ps_table)
+                        entry_table += ps_table.get_string() + "\n"
+
                     elif attr_type == '$LOGGED_UTILITY_STREAM':
-                        entry_table += tablecreation_instance.logged_utility_stream(hex_dump[current_offset:])
+                        lus_table = tablecreation_instance.logged_utility_stream(hex_dump[current_offset:])
+                        all_pretty_tables.append(lus_table)
+                        entry_table += lus_table.get_string() + "\n"
 
                 previous_offset = current_offset
                 current_offset = update_offset(hex_dump, current_offset)
 
-            all_tables += entry_table + "\n\n"
+            if search_name and not extracted_name:
+                continue
+            
+            if search_name and extracted_name and search_name not in extracted_name:
+                continue
 
-        print("     Entry Header for File:")
-        
+            if extracted_name:
+               all_tables += f"\033[91m     Entry Header for File: \033[92m{extracted_name}\033[0m\n"
+
+            else:
+                all_tables += "\033[91m     Entry Header for File: Entry without a name\n \033[0m"
+
+            all_tables += pretty_table.get_string() + "\n\n"  # Add entry header table to all_tables
+            all_tables += entry_table  
+
+        if export_csv:
+            export_to_csv(all_pretty_tables, f"{providedpath}_exported.csv")
+
         return all_tables.rstrip()
     else:
         return 'File not found.'
@@ -161,6 +257,15 @@ def output_results(data, outputpath):
         print(f"An error occurred while writing to the file: {e}")
 
 
+def export_to_csv(tables, filename):
+    with open(filename, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        for table in tables:
+            # Write the headers
+            csvwriter.writerow(table.field_names)
+            # Write the rows
+            for row in table._rows:
+                csvwriter.writerow(row)
 
 
 
@@ -188,22 +293,58 @@ def help():
     print("To export your results, use the -o flag.\n-----./MFTAnalyzer.py C:\\Path\\To\\MFTfile -o C:\\Desired\\Path\\To\\Results-----\n")
     print("To export your results to a CSV, pass the -csv flag (with the -o flag).\n-----./MFTAnalyzer.py C:\\Path\\To\\MFT -csv -o C:\\Desired\\Path\\To\\Results.csv-----\n")
 
-if len(sys.argv) == 1:
-    firstrun()
-elif '-h' in sys.argv:
-    help()
-else:
-    argpath = sys.argv[1]
-    path_output = handle_path(argpath)
-    print(path_output)
 
-if '-o' in sys.argv:
-    try:
-        o_index = sys.argv.index('-o')
-        if o_index + 1 < len(sys.argv):
-            output_path = sys.argv[o_index + 1]
-            output_results(path_output, output_path)
-        else:
-            print("No output path provided after -o.")
-    except ValueError:
-        print("Error processing -o argument.")
+
+
+
+
+def main():
+    search_name = None
+    search_flag = False
+    output_path = None
+    export_csv = False
+
+    if len(sys.argv) == 1:
+        firstrun()
+        sys.exit(0)
+
+    if '-h' in sys.argv:
+        help()
+        sys.exit(0)
+
+    if '-s' in sys.argv:
+        try:
+            s_index = sys.argv.index('-s')
+            if s_index + 1 < len(sys.argv):
+                search_name = sys.argv[s_index + 1]
+                search_flag = True
+            else:
+                print("No search name provided after -s.")
+                sys.exit(1)
+        except ValueError:
+            print("Error processing -s argument.")
+            sys.exit(1)
+
+    if '-o' in sys.argv:
+        try:
+            o_index = sys.argv.index('-o')
+            if o_index + 1 < len(sys.argv):
+                output_path = sys.argv[o_index + 1]
+            else:
+                print("No output path provided after -o.")
+                sys.exit(1)
+        except ValueError:
+            print("Error processing -o argument.")
+            sys.exit(1)
+
+    if '--csv' in sys.argv:
+        export_csv = True
+
+    argpath = sys.argv[1]
+    path_output = handle_path(argpath, search_name if search_flag else None, export_csv)
+    print(path_output)
+    if output_path:
+        output_results(path_output, output_path)
+
+if __name__ == "__main__":
+    main() #script execution
